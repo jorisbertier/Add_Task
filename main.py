5f8ad5ba2dd8dea22cad37f37aba2ac8be92b8d6
Joris Bertier <joris.bertier@gmail.com>
2024-12-24 15:34:19 -0600

    refacto delete task with index

--- main.py
+++ main.py
@@ -31,6 +31,45 @@ def create_task():
 # MENU 3 DELETE A TASK
 def delete_task():
     print('List of tasks, please enter the task to delete corretly')
+
+    with open("tasks.txt", "r", encoding="utf-8") as file:
+        tasks = file.readlines()
+    
+    if not tasks:
+        print('No task available to delete')
+        return
+    
+    for i, task in enumerate(tasks, start=1):
+        print(f'[{i}]-{task}')
+    
+    select_task = input('Please select a task to delete:')
+    # with open("tasks.txt", "w", encoding="utf-8") as file:
+    #     for i, task in enumerate(tasks, start=1):
+    #         if select_task == task.strip():
+    #             task_found = True
+    #             print(f'Deleted task found: {task}')
+    #             continue
+    #         file.write(task)
+    try:
+        task_index = int(select_task) - 1
+        if task_index < 0 or task_index >= len(tasks):
+            print('Invalid task number. Try again')
+            select_task = input('Please select a task to delete:')
+            return
+    except ValueError:
+        print("Please enter a valid number.")
+        select_task = input('Please select a task to delete:')
+        return
+
+    task_delete = tasks.pop(task_index)
+
+    with open("tasks.txt", "w", encoding="utf-8") as file:
+        file.writelines(tasks)
+    print(f"Task deleted : {task_delete.strip()}")
+    
+# Menu 4 MODIFY A TASK
+def modify_task():
+    print('List of tasks, please enter the task to delete corretly')
     see_all_tasks()
 
     select_task = input('Please select a task to delete:')
