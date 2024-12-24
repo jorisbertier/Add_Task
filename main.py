7b941a011df410d04a44a2c570078ee49df52592
Joris Bertier <joris.bertier@gmail.com>
2024-12-24 15:08:44 -0600

    create menu 3 delete a task

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def see_all_tasks():
         print(tasks)
 
 # MENU 2 CREATE A TASK
-def create_a_task():
+def create_task():
     task = input('Please enter a task:')
     while len(task.strip()) == 0:
         task = input('Please enter a task:')
@@ -28,14 +28,36 @@ def create_a_task():
         with open('tasks.txt', 'a', encoding="utf-8") as file:
             file.write(task + '\n' )
 
+# MENU 3 DELETE A TASK
+def delete_task():
+    print('List of tasks, please enter the task to delete corretly')
+    see_all_tasks()
 
+    select_task = input('Please select a task to delete:')
+    task_found = False
+
+    with open("tasks.txt", "r", encoding="utf-8") as file:
+        tasks = file.readlines()
+    with open("tasks.txt", "w", encoding="utf-8") as file:
+        for task in tasks:
+            # print(t.strip())
+            # print(t)
+            if select_task == task.strip():
+                task_found = True
+                print(f'Deleted task found: {task}')
+                continue
+            file.write(task)
+
+    if not task_found:
+        print('Task not found')
+    return
 
 if menu == 1:
     see_all_tasks()
 elif menu == 2:
-    create_a_task()
+    create_task()
 elif menu == 3:
-    print("33333")
+    delete_task()
 elif menu == 4:
     print("44444")
 else:
